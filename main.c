101077031a8b21985ed637f6dd89a970f9872090
root <root@localhost.localdomain>
2017-06-17 04:02:09 -0400

    go

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
-#include "main.h"
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
+#include<netinet/udp.h>   //Provides declarations for udp header
+#include<netinet/tcp.h>   //Provides declarations for tcp header
+#include<netinet/ip.h>    //Provides declarations for ip header
+#include<netinet/if_ether.h>  //For ETH_P_ALL
+#include<net/ethernet.h>  //For ether_header
 
 int main(void)
 {
-   printf("hello world\n"); 
+  int sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
+  unsigned char buffer[1024] = {0};
+  struct sockaddr_in saddr;
+  int saddr_size = sizeof(saddr);
+  int data_size = 0;
+
+  while(data_size == 0) {
+    data_size = recvfrom(sock_raw, buffer, sizeof(buffer), 0, (struct sockaddr *)&saddr, &saddr_size);
+  }
+
+  printf("%u\n", data_size);
+
+  int i;
+  for(i = 0; i < data_size; i++){
+    printf("%02X ", buffer[i]);
+  }
+  printf("\n");
+
+  return 0;
 }
+
